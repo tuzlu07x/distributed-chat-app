cd8143a9db7b526ba6b30f2ba9d96d3fa57fcdb7
Fatih Tuzlu <fatihtuzlu07@gmail.com>
2025-04-22 17:00:57 +0300

    app module revized according to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatModule } from './chat/chat.module';
+import { NotificationsService } from './notifications/notifications.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/distributed-chat'),
+    ChatModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, NotificationsService],
 })
 export class AppModule {}
